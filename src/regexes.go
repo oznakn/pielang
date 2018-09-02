89023fc20de1e940a81433cb6bb0e0b8e37bcab2
Ozan Akın <ozn.akn@gmail.com>
2018-09-02 13:56:42 +0300

    Updated regexes

--- src/regexes.go
+++ src/regexes.go
@@ -1,10 +1,12 @@
 package main
 
 const (
-	COMMENT_FIND_REGEX                 = `((\/\*.*?\*\/)|(\/\/.*?($|\n)))`
+	COMMENT_FIND_REGEX                 = `((\/\*[\s\S]*?\*\/)|(\/\/.*?($|\n)))`
 	NEWLINE_FIND_REGEX                 = `;?[\n\r]`
 	FUNCTION_FIRST_LINE_FIND_REGEX     = `(function .+?\(.*?\).*?\{)`
-	STRINGS_FIND_REGEX                 = `("([^"]|"")*")` // `(["'])(?:(?=(\\?))\2.)*?\1`
+	IF_FIND_REGEX                      = `if\s*\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)\s*\{(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}`
+	WHILE_FIND_REGEX                   = `while\s*\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)\s*\{(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}`
+	STRINGS_FIND_REGEX                 = `("[^"\\\n\r]*(?:\\.[^"\\\n\r]*)*")` + "|" + "(`[^`" + `\\` + "]*(?:" + `\\` + ".[^`" + `\\` + "]*)*`)"
 	IS_VALID_VARIABLE_NAME             = `^[a-zA-Z][a-zA-Z0-9_]+$`
 	FIND_OUTER_PARANTHESIS_GROUP_REGEX = `\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)`
 )

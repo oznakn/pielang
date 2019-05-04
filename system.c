3a8d727b340feec37b0a3265a4d768fadb33fb35
Ozan Akın <ozn.akn@gmail.com>
2019-05-04 23:32:11 +0300

    add input function

--- system.c
+++ system.c
@@ -1,11 +1,16 @@
 #include "system.h"
 
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 
 #include "value.h"
 #include "scope.h"
+#include "utils.h"
+
+
+#define BUFFER_SIZE 100000
 
 
 Value *system_function_print(TupleValue *parameter_values) {
@@ -119,6 +124,54 @@ Value *system_function_max(TupleValue *parameter_values) {
 }
 
 
+Value *system_function_input(TupleValue *parameter_values) {
+  if (parameter_values->length > 0) {
+    StringValue *string_value = (StringValue *) convert_to_string_value(parameter_values->items[0]);
+
+    printf("%s", string_value->string_value);
+
+    free_value((Value *) string_value);
+  }
+
+  char buffer[BUFFER_SIZE];
+  size_t buffer_length = 0;
+
+  char c;
+
+  while (true) {
+    c = getchar_unlocked();
+
+    if (c == EOF || c == '\n') break;
+    else if (c < ' ') continue;
+
+    buffer[buffer_length++] = c;
+  }
+
+  return new_string_value(create_string_from_buffer(buffer, buffer_length), buffer_length);
+}
+
+
+Value *system_function_number(TupleValue *parameter_values) {
+  if (parameter_values->length == 0) return new_null_value();
+
+  Value *value = parameter_values->items[0];
+
+  if (value->value_type == ValueTypeIntegerValue || value->value_type == ValueTypeFloatValue) {
+    return copy_value(value);
+  }
+  else if (value->value_type == ValueTypeStringValue) {
+    long double float_value = strtold(((StringValue *) value)->string_value, NULL);
+
+    if (ceil(float_value) == floor(float_value)) {
+      return new_integer_value((long long int) float_value);
+    }
+    else {
+      return new_float_value(float_value);
+    }
+  }
+
+  return new_integer_value(convert_to_integer(value));
+}
 
 
 void build_system_function(Scope *scope, char *name, Value *value) {
@@ -132,5 +185,7 @@ void build_main_scope(Scope *scope) {
   build_system_function(scope, "print", new_system_function_value(system_function_print));
   build_system_function(scope, "min", new_system_function_value(system_function_min));
   build_system_function(scope, "max", new_system_function_value(system_function_max));
+  build_system_function(scope, "input", new_system_function_value(system_function_input));
+  build_system_function(scope, "number", new_system_function_value(system_function_number));
 }
 
